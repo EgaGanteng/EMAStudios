bbba1a6a415235659249513b7428f1c8639a26b9
Michael William Kinsey <maiksem5@gmail.com>
2014-10-22 22:44:31 +0100

    Revert "Update main.java"
    This reverts commit 167145439855f217a627d8e603fd32a0f10f98c9.

--- JavaApplication1/src/javaapplication1/main.java
+++ JavaApplication1/src/javaapplication1/main.java
@@ -14,6 +14,5 @@ public class main {
     public static void main(String[] args) {
         System.out.println("ADBO");
         System.out.println("Anton");
-        System.out.println("Azzzz");
     }
 }
