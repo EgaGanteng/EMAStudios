ec18d52bd2ba89da0856b045fdfbc308f072e16d
Michael William Kinsey <maiksem5@gmail.com>
2014-11-20 15:59:55 +0700

    ubah nama Chalange jadi challenge saja

---
+++ ChipEMA/src/chipchallenge/ChipChallenge.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package chipchallenge;
+
+import controller.Controller;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import view.Canvas;
+
+/**
+ * Method inti yang menjalankan Game.
+ * @author Ega Prianto
+ */
+public class ChipChallenge {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("EMA Chip Chalenge");
+        Canvas ra = new Canvas();
+        Controller ctrl = new Controller(ra);
+        ra.setCtrl(ctrl);
+        Thread thread = new Thread((Controller)ctrl);
+        thread.start();
+        f.getContentPane().add("Center", ra);
+        f.pack();
+        f.setSize(new Dimension(1024, 600));
+        f.setVisible(true);
+    }
+}
